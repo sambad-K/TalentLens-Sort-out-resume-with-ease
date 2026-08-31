a3f64826d9c8e5613eea0026c63d76c76838a5fa
sambaf-K <sambadkhatiwada939@gmail.com>
2026-08-31 10:51:54 +0545

    feat-connect to remote neon db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -169,3 +170,16 @@ MAILERS = {
 }
 
 DEFAULT_FROM_EMAIL = EMAIL_SENDER
+
+
+
+
+
+
+DATABASES = {
+    "default": dj_database_url.parse(
+        config("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
